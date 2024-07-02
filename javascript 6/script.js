f650414a7d6dc08cb77f938041bffaf7c367d5ed
Sida14 <164236903+Sida14@users.noreply.github.com>
2024-07-02 15:25:10 +0400

    Add files via upload

---
+++ javascript 6/script.js
@@ -0,0 +1,23 @@
+ function checknumbers(num) {
+    if (num > 10) {
+        console.log("num is greater than 10");
+    } else {
+        console.log("num is less than 10 or equal")
+    }
+ }
+
+ checknumbers(5)
+
+
+ function greet(name) {
+    console.log( "Hello, name!")
+ }
+
+ greet("everyone")
+
+
+ function sayhello(yup) {
+    console.log("Hello, world!")
+ }
+
+ sayhello("yup")
